e81542b89cc991ed9de2b1a17dc5206b321e0b91
ACCRAWEB <48293588+ACCRAWEB@users.noreply.github.com>
2019-03-07 10:09:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
 // Generate random room name if needed
-//if (!location.hash) {
-//  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
-//}
-location.hash = "#dc3ca6";
+if (!location.hash) {
+  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
 const roomHash = location.hash.substring(1);
 
 // TODO: Replace with your own channel ID
