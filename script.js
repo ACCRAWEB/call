dee028dc5edb43a512e5ac9b610b2f68c870e601
ACCRAWEB <48293588+ACCRAWEB@users.noreply.github.com>
2019-03-07 11:48:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-
-// Generate random room name if needed
-//if (!location.hash) {
-//  location.hash = Math.floor(Math.random() * 0xFFFFFF).toString(16);
-//}
-const roomHash = "9ac74b"//location.hash.substring(1);
+const roomHash = "9ac74b";
 console.log(roomHash);
 
 
